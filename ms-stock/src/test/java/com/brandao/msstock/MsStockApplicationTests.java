266f9ecaa903e5e69e00409285f961a5557308b0
tauabrandao <brandao.taua@gmail.com>
2021-06-10 21:48:51 -0300

    [ms-stock] Project started

---
+++ ms-stock/src/test/java/com/brandao/msstock/MsStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandao.msstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
