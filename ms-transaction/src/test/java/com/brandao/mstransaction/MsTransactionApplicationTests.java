d581153906e9ae9e9edf09f3320cac39546b8c39
tauabrandao <brandao.taua@gmail.com>
2021-06-12 13:41:14 -0300

    [ms-transaction] project started

---
+++ ms-transaction/src/test/java/com/brandao/mstransaction/MsTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandao.mstransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
